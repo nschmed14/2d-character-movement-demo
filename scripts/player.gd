c6193e8b7ba226f072b96407dda7cc7c17c8f43e
nschmed14 <naschmedding@gmail.com>
2026-07-08 04:32:28 -0500

    Fixed air deceleration

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,15 @@ extends CharacterBody2D
 
 @export var walk_speed = 450.0
 @export var run_speed = 1050.0
+@export var run_decel = 10.0
 @export_range(0, 1) var deceleration = 0.1
 @export_range(0, 1) var acceleration = 0.1
 
 @export var jump_force = -700
 @export_range(0,1) var decelerate_on_jump_release = 0.5
 
-@export var dash_speed = 2000.0
+
+@export var dash_speed = 1500.0
 @export var dash_max_distance = 300.0
 @export var dash_curve : Curve
 @export var dash_cooldown = 1.0
@@ -29,33 +31,36 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_wall()):
 		velocity.y = jump_force
 
-
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= decelerate_on_jump_release
 
-		
-	var speed
+	# Determine target speed
+	var target_speed = walk_speed
 	if Input.is_action_pressed("run") and is_on_floor():
-		speed = run_speed
-		print('1')
-	elif velocity.x > walk_speed:
-		speed = move_toward(velocity.x, walk_speed, walk_speed * deceleration)
-		print('2')
-	elif velocity.x < -(walk_speed):
-		speed = move_toward(velocity.x, walk_speed, walk_speed * deceleration)
-		print(velocity.x)
-	else:
-		speed = walk_speed
-		print('3')
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		target_speed = run_speed
+	
+	# Get the input direction
 	var direction := Input.get_axis("left", "right")
+	
+	# Handle horizontal movement
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
+		# Moving
+		if is_on_floor():
+			# Ground acceleration
+			velocity.x = move_toward(velocity.x, direction * target_speed, target_speed * acceleration)
+		else:
+			# Air acceleration
+			velocity.x = move_toward(velocity.x, direction * target_speed, target_speed * 0.05)
 	else:
-		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
-		
+		# Not moving - apply deceleration
+		if is_on_floor():
+			# Ground deceleration
+			velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
+		else:
+			# Air deceleration
+			velocity.x = move_toward(velocity.x, 0, walk_speed * 0.03)
+	
+	# Handle dash
 	if Input.is_action_just_pressed("dash") and direction and not is_dashing and dash_timer <= 0:
 		is_dashing = true
 		dash_start_position = position.x
